ea193998b26a1c0074b83294114202cb892f878a
mm12346 <xoctell338@gmail.com>
2025-06-14 00:08:35 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Investment Tracker PWA
 
-const CACHE_NAME = 'investment-tracker-cache-v5';
+const CACHE_NAME = 'investment-tracker-cache-v6';
 const urlsToCache = [
   './',
   './index.html', // Assuming your main file is index.html
